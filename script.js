09b407bfd9a4b9cea87e384ab7647f4e63c730a5
Husnain Zahid <husnainzahid214@gmail.com>
2022-09-04 23:49:16 +0500

    "first commit"

---
+++ script.js
@@ -0,0 +1,31 @@
+const root = document.documentElement
+const lightTheme = document.querySelector ("#lightTheme")
+const darkTheme = document.querySelector ("#darkTheme")
+const logo = document.querySelector (".logo")
+const hamMenu = document.querySelector (".ham-menu")
+
+lightTheme.addEventListener ("click", setLightTheme)
+darkTheme.addEventListener ("click", setDarkTheme)
+
+function setLightTheme () {
+    root.style.setProperty ("--body-bg", "#eaf3f1")
+    root.style.setProperty ("--dark-green", "#0A211E")
+    logo.setAttribute ("src", "./images/logo.svg")
+}
+
+function setDarkTheme () {
+    root.style.setProperty ("--body-bg", "#0A211E")
+    root.style.setProperty ("--dark-green", "#eaf3f1")
+    logo.setAttribute ("src", "./images/logo-dark.svg")
+}
+
+function toggleNav () {
+    if (hamMenu.className.includes ("hamAnimationShow")) {
+        hamMenu.classList.remove ("hamAnimationShow")
+        hamMenu.classList.add ("hamAnimationHide")
+    }
+    else {
+        hamMenu.classList.add ("hamAnimationShow")
+        hamMenu.classList.remove ("hamAnimationHide")
+    }
+}
